e4afce7f3c7dbe1e8c27e080d50e783cbf53b99a
Eric Fournier <ericfournier2@yahoo.ca>
2018-03-07 13:41:38 -0500

    Shuffle files around to split Mouse and Bovine.

--- scripts/bovine/cut1.sh
+++ scripts/bovine/cut1.sh
@@ -2,7 +2,7 @@ for i in 1 2 3 4 5
 do
     R1="HI.0588.00"$i.$i"_R1.fastq.gz"
     R2="HI.0588.00"$i.$i"_R2.fastq.gz"
-    outputdir=output_v2
+    outputdir=output/bovine
     script=jobs/$i.cut1.sh
     mkdir -p $outputdir/cut1
     mkdir -p jobs
@@ -14,7 +14,7 @@ do
 #PBS -l walltime=5:00:00
 #PBS -l nodes=1:ppn=8
 module load mugqic/python/2.7.8
-cutadapt -a AGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATTAGATCGGAAGAGCTCGTATGCCGTCTTCTGCTTG -o $outputdir/cut1/$i.R1.fastq.gz -p $outputdir/cut1/$i.R2.fastq.gz raw/$R1 raw/$R2
+cutadapt -a AGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATTAGATCGGAAGAGCTCGTATGCCGTCTTCTGCTTG -o $outputdir/cut1/$i.R1.fastq.gz -p $outputdir/cut1/$i.R2.fastq.gz raw/bovine/$R1 raw/bovine/$R2
 EOF
     workdir=`pwd`
     jobid=`qsub $script -o $script.stdout -e $script.stderr -d $workdir`
--- scripts/bovine/demultiplex.sh
+++ scripts/bovine/demultiplex.sh
@@ -2,9 +2,9 @@
 for i in 1 2 3 4 5
 do
     # Generate index files for fastq-multx
-    grep -e "^$i" input/Samples.txt | awk '{ print $2"."$3"\t"$5}' > input/$i.sample.index
+    grep -e "^$i" input/bovine/Samples.txt | awk '{ print $2"."$3"\t"$5}' > input/bovine/$i.sample.index
 
-    outputdir=output_v2
+    outputdir=output/bovine
     mkdir -p $outputdir/demultiplexed
     
     # Generate and submit demultiplexing job.    
@@ -18,7 +18,7 @@ do
 module load mugqic/python/2.7.8
 cd `pwd`
 module load mugqic/ea-utils/1.1.2-537
-fastq-multx input/$i.sample.index $outputdir/cut2/$i.R1.fastq.gz $outputdir/cut2/$i.R2.fastq.gz -o $outputdir/demultiplexed/%.R1.fastq.gz $outputdir/demultiplexed/%.R2.fastq.gz
+fastq-multx input/bovine/$i.sample.index $outputdir/cut2/$i.R1.fastq.gz $outputdir/cut2/$i.R2.fastq.gz -o $outputdir/demultiplexed/%.R1.fastq.gz $outputdir/demultiplexed/%.R2.fastq.gz
 EOF
     workdir=`pwd`
     previousjob=`echo jobs/$i.cut1.sh.jobid`
